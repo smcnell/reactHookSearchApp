ac3b8d2b5f3370cc9a377f54b876ca0293c2a0dc
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-21 17:39:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState("react hooks");
+    const [loading, setLoading] = useState(false);
+    const searchInputRef = useRef();
+
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+    const getData = async () => {
+        setLoading(true);
+        const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+        setResults(response.data.hits)
+        setLoading(false);
+    }
+
+    const handleSearch = event => {
+        event.preventDefault();
+        getData();
+    }
+
+    const handleClearSearch = () => {
+        setQuery("")
+        searchInputRef.current.focus();
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <form  onSubmit={handleSearch}>
+            <input type="text"
+                   value={query}
+                   ref={searchInputRef}
+                   onChange={event => setQuery(event.target.value)}/>
+            <button type="submit">Search</button>
+            <button type="button" onClick={handleClearSearch}>Clear</button>
+
+        </form>
+        {loading ? (
+            <div>Loading results</div>
+            ) : (
+            <ul>
+        {results.map(result => (
+            <li key={result.objectId}>
+                <a href={result.url}>{result.title}</a>
+            </li>
+        ))}
+        </ul>
+        )}
+
+    </>
   );
 }
-
-export default App;
